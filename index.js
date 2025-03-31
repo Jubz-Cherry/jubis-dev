fd306fc8700a1af6a694c8faeaf528833841a59c
Jubz-Cherry <juliapnascimento15@gmail.com>
2025-03-31 19:01:51 -0300

    more upgrades

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require("express")
 const app = express();
 
-
 app.set('view engine','ejs');
+app.use(express.static('public'));
 
-
-
-app.get("/",(req,res)=>{
+app.get("/",(req,res) => {
     res.render("index")
-})
+});
+
+
+
+app.get("/perguntar",(req,res) => {
+    res.render("perguntar")
+});
 
 
 
