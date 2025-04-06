6e7dc8089f5c7ec218e8c237ae20fef3b685d476
Jubz-Cherry <juliapnascimento15@gmail.com>
2025-04-06 20:36:45 -0300

    add tag and partials

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const express = require("express")
+const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 app.set('view engine','ejs');
 app.use(express.static('public'));
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
 app.get("/",(req,res) => {
     res.render("index")
 });
-
-
 
 
 app.get("/perguntar",(req,res) => {
@@ -16,6 +19,9 @@ app.get("/perguntar",(req,res) => {
 });
 
 
+app.post("/salvarpergunta",(req,res) => {
+    res.send("Formulário recebido!");
+});
 
 
 app.listen(8080,(
